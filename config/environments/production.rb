324c96f7fd89047b678135b69fa32f9465471687
Michael Hoevel <michael.hoevel.duisburg@gmail.com>
2016-11-20 13:57:41 +0100

    fix problem with sendnmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,10 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "enterpriseapp_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = {:host => 'smtp.sendgrid.net', port:587}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
