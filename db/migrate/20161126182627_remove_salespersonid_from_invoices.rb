d19c6185f229ad247242fddfa53f895412581946
Michael Hoevel <michael.hoevel.duisburg@gmail.com>
2016-11-26 19:36:53 +0100

    Added employee and dropdown for employee in invoices

---
+++ db/migrate/20161126182627_remove_salespersonid_from_invoices.rb
@@ -0,0 +1,6 @@
+class RemoveSalespersonidFromInvoices < ActiveRecord::Migration[5.0]
+  def change
+    remove_column :invoices, :salesperson_id, :integer
+    add_column :invoices, :employee_id, :integer
+  end
+end
